cc47dc689eaca10f3520131ff1de0d539445ff17
freecontrolreal <109272347+freecontrolreal@users.noreply.github.com>
2022-07-14 12:02:50 -1000

    Update MainModule.lua

--- src/modules/MainModule.lua
+++ src/modules/MainModule.lua
@@ -9,3 +9,58 @@ end
 function FormatNumToStyle(arg)
     return tostring(arg):reverse():gsub("%d%d%d", "%1,"):reverse():gsub("^,", "")
 end
+
+function FormAirlock(hieght)
+    if workspace:FindFirstChild("Float") then
+        workspace:FindFirstChild("Float"):Destroy()
+    end
+    for i,v in pairs(game:service"Players".LocalPlayer.Character:FindFirstChildWhichIsA('Humanoid'):GetPlayingAnimationTracks()) do
+        if v.Name == "Float" then
+            v:Stop()
+        end
+    end
+    game:service"Players".LocalPlayer.Character.HumanoidRootPart.Anchored = false
+    local Part = Instance.new("Part",workspace)
+    Part.Transparency = 1
+    Part.Size = Vector3.new(5,1,5)
+    Part.CFrame = game:service"Players".LocalPlayer.Character.HumanoidRootPart.CFrame*CFrame.new(0,tonumber(hieght),0)
+    Part.Anchored = true
+    game:service"Players".LocalPlayer.Character.HumanoidRootPart.CFrame=game:service"Players".LocalPlayer.Character.HumanoidRootPart.CFrame*CFrame.new(0,tonumber(hieght)+1,0)
+    wait(1)
+    local Anim = Instance.new("Animation",workspace)
+    Anim.Name = "Float"
+    Anim.AnimationId = 'rbxassetid://3541114300'
+    game:service"Players".LocalPlayer.Character.Humanoid:LoadAnimation(Anim):Play()
+    game:service"Players".LocalPlayer.Character.HumanoidRootPart.Anchored = true
+    Part:Destroy()
+end
+
+function CheckForPlr(userid)
+    for i,v in pairs(game:service"Players":GetChildren()) do
+        if v.UserId == userid then
+            return true
+        end
+    end
+    return false
+end
+
+function ReformAltList()
+    if Alts then
+        local new = {}
+        for _, v2 in pairs(Alts) do
+            if CheckForPlr(v2) == true then
+                table.insert(new,v2)
+            end
+        end
+        return new
+    end
+end
+
+function FormLine(anchor,listofcords)
+    local list = ReformAltList()
+    for i,v in pairs(list) do
+        if v == game:service"Players".LocalPlayer.UserId then
+            game:service"Players".LocalPlayer.Character.HumanoidRootPart.CFrame = anchor.CFrame*CFrame.new(listofcords[i]:split(",")[1],0,0)
+        end
+    end
+end
