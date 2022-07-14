3e8bc44d520f79c05efc17a12608ee5089a57332
freecontrolreal <109272347+freecontrolreal@users.noreply.github.com>
2022-07-13 23:03:08 -1000

    Update backgroundstuff.lua

--- src/modules/backgroundstuff.lua
+++ src/modules/backgroundstuff.lua
@@ -1,23 +1,3 @@
-for _, plr in pairs(workspace.Players:GetChildren()) do
-    print('reading')
-    repeat wait() until plr.Parent ~= and plr:FindFirstChild("DataFolder")
-    if game:service"Players"[plr.Name].UserId ~= game:service"Players".LocalPlayer.UserId and game:service"Players"[plr.Name].UserId ~= Settings['host'] and table.find(Alts,plr.UserId) then
-        for i,v in pairs(plr:GetChildren()) do
-            v:Destroy()
-        end
-    end
-end
-
-game:service"Workspace".Players.ChildAdded:Connect(function(plr)
-    print('reading')
-    repeat wait() until plr.Parent ~= and plr:FindFirstChild("DataFolder")
-    if game:service"Players"[plr.Name].UserId ~= game:service"Players".LocalPlayer.UserId and game:service"Players"[plr.Name].UserId ~= Settings['host'] and table.find(Alts,plr.UserId) then
-        for i,v in pairs(plr:GetChildren()) do
-            v:Destroy()
-        end
-    end
-end)
-
 game:service"Players".LocalPlayer.Idled:Connect(function()
     game:service"VirtualUser":Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
     wait(1)
