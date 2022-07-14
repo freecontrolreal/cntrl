d440328887387c940d82ab14c119ef9b14b12c76
freecontrolreal <109272347+freecontrolreal@users.noreply.github.com>
2022-07-13 22:10:54 -1000

    Update backgroundstuff.lua

--- src/modules/backgroundstuff.lua
+++ src/modules/backgroundstuff.lua
@@ -1,3 +1,11 @@
+for _, plr in pairs(workspace.Players:GetChildren()) do
+    if game:service"Players"[plr.Name].UserId == game:service"Players".LocalPlayer.UserId then return end
+    if game:service"Players"[plr.Name].UserId == Settings['host'] then return end
+    if not table.find(Alts,plr.UserId) then return end
+    repeat wait() until plr.Parent ~= nil and plr.Character:FindFirstChild("HumanoidRootPart")
+    plr:Destroy()
+end
+
 game:service"Workspace".Players.ChildAdded:Connect(function(plr)
     if game:service"Players"[plr.Name].UserId == game:service"Players".LocalPlayer.UserId then return end
     if game:service"Players"[plr.Name].UserId == Settings['host'] then return end
