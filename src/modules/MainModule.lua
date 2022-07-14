f19c8dbbe5998fa0f2c38a987922741f0d66a6bd
freecontrolreal <109272347+freecontrolreal@users.noreply.github.com>
2022-07-13 23:03:18 -1000

    Update MainModule.lua

--- src/modules/MainModule.lua
+++ src/modules/MainModule.lua
@@ -9,3 +9,25 @@ end
 function FormatNumToStyle(arg)
     return tostring(arg):reverse():gsub("%d%d%d", "%1,"):reverse():gsub("^,", "")
 end
+
+function HeadLockDisattachment()
+    if workspace:FindFirstChild("HeadLockPiece") then
+        workspace:FindFirstChild("HeadLockPiece"):Destroy()
+    end
+    if workspace:FindFirstChild("HeadLockWelder") then
+        workspace:FindFirstChild("HeadLockWelder"):Destroy()
+    end
+end
+
+function HeadLockAttachment()
+    HeadLockDisattachment()
+    local Part = Instance.new("Part",workspace)
+    local Weld = Instance.new("Weld",workspace)
+    Part.Position = game.Players.LocalPlayer.Character.Head.Position
+    Part.Transparency = 1
+    Part.Massless = false
+    Part.CanCollide = false
+    Weld.Part0 = game.Players.LocalPlayer.Character.Head
+    Weld.Part1 = Part
+    Part.Anchored = true
+end
